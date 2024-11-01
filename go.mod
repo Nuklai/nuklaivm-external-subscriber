4b1cea372017bdad944a0d8dd55ea57e831d1ca4
Kiran Pachhai <kiran.pachhai@gmail.com>
2024-11-01 18:38:10 -0400

    Updated to print more info about each block and txes

--- go.mod
+++ go.mod
@@ -3,13 +3,76 @@ module github.com/nuklai/nuklaivm-external-subscriber
 go 1.22.5
 
 require (
+github.com/nuklai/nuklaivm v0.1.3-0.20241031182233-374630b3e50d
+	github.com/ava-labs/hypersdk v0.0.18-0.20241018181853-22241f53b9ff
 	google.golang.org/grpc v1.67.1
 	google.golang.org/protobuf v1.35.1
 )
 
 require (
+	filippo.io/edwards25519 v1.0.0 // indirect
+	github.com/BurntSushi/toml v1.4.0 // indirect
+	github.com/DataDog/zstd v1.5.2 // indirect
+	github.com/ava-labs/avalanchego v1.11.12-rc.2.0.20241001202925-f03745d187d0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/bytecodealliance/wasmtime-go/v14 v14.0.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.2.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/cockroachdb/errors v1.9.1 // indirect
+	github.com/cockroachdb/logtags v0.0.0-20230118201751-21c54148d20b // indirect
+	github.com/cockroachdb/pebble v0.0.0-20230928194634-aa077af62593 // indirect
+	github.com/cockroachdb/redact v1.1.3 // indirect
+	github.com/cockroachdb/tokenbucket v0.0.0-20230807174530-cc333fc44b06 // indirect
+	github.com/getsentry/sentry-go v0.18.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
+	github.com/google/btree v1.1.2 // indirect
+	github.com/google/renameio/v2 v2.0.0 // indirect
+	github.com/gorilla/rpc v1.2.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.16.0 // indirect
+	github.com/hdevalence/ed25519consensus v0.2.0 // indirect
+	github.com/klauspost/compress v1.15.15 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/nbutton23/zxcvbn-go v0.0.0-20180912185939-ae427f1e4c1d // indirect
+	github.com/near/borsh-go v0.3.1 // indirect
+
+	github.com/onsi/ginkgo/v2 v2.13.1 // indirect
+	github.com/openzipkin/zipkin-go v0.4.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.16.0 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.10.1 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
+	github.com/supranational/blst v0.3.11 // indirect
+	go.opentelemetry.io/otel v1.22.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.22.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.22.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.22.0 // indirect
+	go.opentelemetry.io/otel/exporters/zipkin v1.11.2 // indirect
+	go.opentelemetry.io/otel/metric v1.22.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.22.0 // indirect
+	go.opentelemetry.io/otel/trace v1.22.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/crypto v0.26.0 // indirect
+	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
 	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sync v0.8.0 // indirect
 	golang.org/x/sys v0.24.0 // indirect
+	golang.org/x/term v0.23.0 // indirect
 	golang.org/x/text v0.17.0 // indirect
+	gonum.org/v1/gonum v0.11.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240814211410-ddb44dafa142 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,37 +2,135 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
 
-	pb "github.com/nuklai/nuklaivm-external-subscriber/pb/externalsubscriber"
+	"github.com/ava-labs/hypersdk/chain"
+	"github.com/ava-labs/hypersdk/codec"
+	pb "github.com/ava-labs/hypersdk/proto/pb/externalsubscriber"
+	"github.com/ava-labs/hypersdk/utils"
+	"github.com/nuklai/nuklaivm/vm"
 )
 
 // Server implements the ExternalSubscriberServer
 type Server struct {
 	pb.UnimplementedExternalSubscriberServer
+	parser chain.Parser
 }
 
 // Initialize receives genesis data for initialization
 func (s *Server) Initialize(ctx context.Context, req *pb.InitializeRequest) (*emptypb.Empty, error) {
 	fmt.Println("Initializing External Subscriber with genesis data...")
-	fmt.Printf("Genesis Data: %x\n", req.GetGenesis())
+
+	// Decode genesis data
+	genesisData := req.GetGenesis()
+	var parsedGenesis map[string]interface{}
+	if err := json.Unmarshal(genesisData, &parsedGenesis); err != nil {
+		fmt.Println("Error parsing genesis data:", err)
+	} else {
+		fmt.Printf("Genesis Data (parsed): %v\n", parsedGenesis)
+	}
+
+	// Create parser from genesis bytes
+	parser, err := vm.CreateParser(genesisData)
+	if err != nil {
+		fmt.Println("Error creating parser:", err)
+		return nil, err
+	}
+	s.parser = parser
+
 	return &emptypb.Empty{}, nil
 }
 
 // AcceptBlock prints block data whenever a block is accepted
 func (s *Server) AcceptBlock(ctx context.Context, req *pb.BlockRequest) (*emptypb.Empty, error) {
 	fmt.Println("Received a new block:")
-	fmt.Printf("Block Data: %x\n", req.GetBlockData())
+
+	// Extract and print raw BlockData
+	blockData := req.GetBlockData()
+
+	// Attempt to unmarshal the executed block using UnmarshalExecutedBlock
+	executedBlock, err := chain.UnmarshalExecutedBlock(blockData, s.parser)
+	if err != nil {
+		fmt.Printf("Error parsing block data: %v\n", err)
+		return &emptypb.Empty{}, nil
+	}
+
+	// Extract and print details from the unmarshaled block
+	blk := executedBlock.Block
+	fmt.Printf("Block ID: %s\n", executedBlock.BlockID)
+	fmt.Printf("Parent ID: %s\n", blk.Prnt)
+	fmt.Printf("Timestamp: %s\n", time.UnixMilli(blk.Tmstmp).Format(time.RFC3339))
+	fmt.Printf("Height: %d\n", blk.Hght)
+	fmt.Printf("State Root: %s\n", blk.StateRoot)
+	fmt.Printf("Unit Prices: %+v\n", executedBlock.UnitPrices)
+
+	// Print detailed transaction information
+	if len(blk.Txs) > 0 {
+		fmt.Printf("Transactions: %d\n", len(blk.Txs))
+		for i, tx := range blk.Txs {
+			fmt.Printf("  Transaction %d:\n", i+1)
+			fmt.Printf("    ID: %s\n", tx.ID())
+			fmt.Printf("    Sponsor: %s\n", tx.Sponsor())
+			fmt.Printf("    Max Fee: %d\n", tx.MaxFee())
+
+			// Display actions and their types
+			if len(tx.Actions) > 0 {
+				fmt.Printf("    Actions: %d\n", len(tx.Actions))
+				for j, action := range tx.Actions {
+					fmt.Printf("      Action %d Type ID: %d\n", j+1, action.GetTypeID())
+					// Additional details for each action can be added here
+				}
+			} else {
+				fmt.Println("    No actions in transaction.")
+			}
+		}
+	} else {
+		fmt.Println("No transactions in block.")
+	}
+
+	// Print detailed output
+	if len(executedBlock.Results) > 0 {
+		processResult(executedBlock.Results[0])
+	}
+
 	return &emptypb.Empty{}, nil
 }
 
+// Helper function to process the transaction result
+func processResult(result *chain.Result) error {
+	if result != nil && result.Success {
+		utils.Outf("{{green}}fee consumed:{{/}} %s NAI\n", utils.FormatBalance(result.Fee))
+
+		// Use NewReader to create a Packer from the result output
+		packer := codec.NewReader(result.Outputs[0], len(result.Outputs[0]))
+		r, err := vm.OutputParser.Unmarshal(packer)
+		if err != nil {
+			return err
+		}
+
+		// Assert the output to the expected type
+		output, ok := r.(interface{})
+		if !ok {
+			return errors.New("failed to assert typed output to expected result type")
+		}
+
+		// Output the results
+		utils.Outf("{{green}}output: {{/}} %+v\n", output)
+	}
+	return nil
+}
+
 func main() {
-	const port = "0.0.0.0:50051"
+	const port = ":50051"
+
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("Failed to listen on port %s: %v", port, err)
